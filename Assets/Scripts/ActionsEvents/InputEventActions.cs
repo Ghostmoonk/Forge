044bbc956936001a4aabf1042fcc9321eb387014
Ghostmoonk <jeremy.tapia30@hotmail.com>
2020-02-01 02:11:34 +0100

    Debut InputEvent

---
+++ Assets/Scripts/ActionsEvents/InputEventActions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputEventActions : MonoBehaviour
+{
+    InputEvent inputEvent;
+
+    private void Start()
+    {
+        inputEvent = transform.parent.GetComponent<InputEvent>();
+    }
+    public void ChangeSucceedableState(SucceedableState state)
+    {
+        inputEvent.succeedState = state;
+    }
+
+    public void EndCircleAnim()
+    {
+        inputEvent.succeedState = SucceedableState.FAILABLE;
+    }
+}
---
+++ Assets/Scripts/Manager/_MGR_Scenes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _MGR_Scenes : MonoBehaviour
+{
+    private static _MGR_Scenes _instance;
+    public static _MGR_Scenes Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
