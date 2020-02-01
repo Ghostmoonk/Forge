f32522143f65f7c3a48525a9a6702738cf928126
Ghostmoonk <jeremy.tapia30@hotmail.com>
2020-02-01 23:49:26 +0100

    Conveyor defiler + destroyer d'items

---
+++ Assets/Scripts/ConveyorDefiler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorDefiler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 firstConveyorPos;
+    Vector3 secondLastConveyorPos;
+    private void Start()
+    {
+        firstConveyorPos = transform.GetChild(0).transform.position;
+        secondLastConveyorPos = transform.GetChild(1).transform.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.GetChild(0).transform.position.x >= secondLastConveyorPos.x)
+        {
+            transform.GetChild(transform.childCount - 1).transform.position = firstConveyorPos;
+            transform.GetChild(transform.childCount - 1).SetAsFirstSibling();
+        }
+    }
+}
--- Assets/Scripts/ItemProvider.cs
+++ Assets/Scripts/ItemProvider.cs
@@ -9,19 +9,86 @@ public enum PatternDifficulty
 
 public class ItemProvider : MonoBehaviour
 {
-    int maxItemsInQueue;
-    int totalItemsInLevel;
-    GameObject[] easyPatternPrefabs;
-    GameObject[] normalPatternPrefabs;
-    GameObject[] hardPatternPrefabs;
+    public int maxItemsInQueue;
+    public int totalItemsInLevel;
+    public int remainingItemsInLevel;
+    [SerializeField] GameObject[] easyPatternPrefabs;
+    [SerializeField] GameObject[] normalPatternPrefabs;
+    [SerializeField] GameObject[] hardPatternPrefabs;
 
+    [SerializeField] Transform spawnPosition;
     GameObject itemsContainer;
 
-    float offsetBetweenItems;
+    [SerializeField] float offsetBetweenItems;
 
     private void Start()
     {
         itemsContainer = GameObject.FindGameObjectWithTag("ItemContainer");
+        remainingItemsInLevel = totalItemsInLevel - itemsContainer.transform.childCount;
+        Debug.Log(remainingItemsInLevel);
+    }
+
+    private void Update()
+    {
+        if (Mathf.Abs(spawnPosition.transform.position.x - itemsContainer.transform.GetChild(itemsContainer.transform.childCount - 1).transform.position.x) > offsetBetweenItems)
+        {
+            if (remainingItemsInLevel > 0)
+            {
+                Debug.Log("I must intantiate !");
+                InstantiateItem(PickRandomDifficulty());
+                remainingItemsInLevel--;
+            }
+        }
+    }
+
+    private PatternDifficulty PickRandomDifficulty()
+    {
+        int randomPick = Random.Range(0, 100);
+        if (remainingItemsInLevel > totalItemsInLevel * 2 / 3)
+        {
+            if (randomPick < 70)
+            {
+                return PatternDifficulty.EASY;
+            }
+            else if (randomPick < 90)
+            {
+                return PatternDifficulty.NORMAL;
+            }
+            else
+            {
+                return PatternDifficulty.HARD;
+            }
+        }
+        else if (remainingItemsInLevel > totalItemsInLevel * 1 / 2)
+        {
+            if (randomPick < 40)
+            {
+                return PatternDifficulty.EASY;
+            }
+            else if (randomPick < 80)
+            {
+                return PatternDifficulty.NORMAL;
+            }
+            else
+            {
+                return PatternDifficulty.HARD;
+            }
+        }
+        else
+        {
+            if (randomPick < 20)
+            {
+                return PatternDifficulty.EASY;
+            }
+            else if (randomPick < 60)
+            {
+                return PatternDifficulty.NORMAL;
+            }
+            else
+            {
+                return PatternDifficulty.HARD;
+            }
+        }
     }
 
     public void InstantiateItem(PatternDifficulty difficulty)
@@ -30,16 +97,15 @@ public class ItemProvider : MonoBehaviour
         switch (difficulty)
         {
             case PatternDifficulty.EASY:
-                patternToInstantiate = Instantiate(easyPatternPrefabs[Random.Range(0, easyPatternPrefabs.Length - 1)]);
-                patternToInstantiate.transform.parent = itemsContainer.transform;
+                patternToInstantiate = Instantiate(easyPatternPrefabs[Random.Range(0, easyPatternPrefabs.Length - 1)], spawnPosition.position, Quaternion.identity, itemsContainer.transform);
                 _MGR_GameManager.Instance.AddItemPaternInQueue(patternToInstantiate.GetComponent<PatternItem>());
                 break;
             case PatternDifficulty.NORMAL:
-                patternToInstantiate = Instantiate(normalPatternPrefabs[Random.Range(0, normalPatternPrefabs.Length - 1)]);
+                patternToInstantiate = Instantiate(normalPatternPrefabs[Random.Range(0, easyPatternPrefabs.Length - 1)], spawnPosition.position, Quaternion.identity, itemsContainer.transform);
                 _MGR_GameManager.Instance.AddItemPaternInQueue(patternToInstantiate.GetComponent<PatternItem>());
                 break;
             case PatternDifficulty.HARD:
-                patternToInstantiate = Instantiate(hardPatternPrefabs[Random.Range(0, hardPatternPrefabs.Length - 1)]);
+                patternToInstantiate = Instantiate(hardPatternPrefabs[Random.Range(0, easyPatternPrefabs.Length - 1)], spawnPosition.position, Quaternion.identity, itemsContainer.transform);
                 _MGR_GameManager.Instance.AddItemPaternInQueue(patternToInstantiate.GetComponent<PatternItem>());
                 break;
             default:
