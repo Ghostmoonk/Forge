162b4743844e6e913fdd1f1da0e2579513aedca7
Yann Roirand <y.roirand@gmail.com>
2020-02-01 17:47:31 +0100

    scoring test scene ready

--- Assets/Scripts/Manager/_MGR_ScoreManager.cs
+++ Assets/Scripts/Manager/_MGR_ScoreManager.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MGR_ScoreManager : MonoBehaviour
+public class _MGR_ScoreManager : MonoBehaviour
 {
     //GameScore Display
     //Game Score is kept here
-    public static int gameScore;
+    public static int currentGameScore;
 
     //HighScores Display
     //High Score are kept here
-    public static int[] HighScores = new int[11];
+    public static int[] highScoresBoard = new int[11];
 
     //GameLaunch Trigger
     //Resets all HighScores to 0
@@ -19,29 +20,29 @@ public class MGR_ScoreManager : MonoBehaviour
     {
         for (int i=0; i<11; i++)
         {
-            highScores[i]=0;
+            highScoresBoard[i]=0;
         }
     }
 
     //GameStart Trigger
     //Resets GameScore to 0
     //Trigger upon gameStart [startGame ButtonDown]
-    public static void ResetScore()
+    public static void ResetGameScore()
     {
-        gameScore = 0;
+        currentGameScore = 0;
     }
 
     //Score Increment
     //Imput was pressed, scoring happened
     //New extra-points to be sent in package
     //Trigger upon ~InputReseived [scoring needs to have been calculated]
-    public static int[] ScoringBundle(int scoring)
+    public static int[] DefinedScoringBundle(int scoreBundle)
     {
-        int[] increaseScore = new int[2];
-        increaseScore.[0]=gameScore;
-        increaseScore.[1]=gameScore + scoring;
-        gameScore = increaseScore[1];
-        return increaseScore;
+        int[] oldAndNewGameScore = new int[2];
+        oldAndNewGameScore[0]=currentGameScore;
+        oldAndNewGameScore[1]=currentGameScore + scoreBundle;
+        currentGameScore = oldAndNewGameScore[1];
+        return oldAndNewGameScore;
     }
 
     //EndGame Trigger
@@ -54,55 +55,55 @@ public class MGR_ScoreManager : MonoBehaviour
         //INT 11 : NUMBER OF THE INT TO HIGHLIGHT (=last game score)
     public static int[] GameOver()
     {
-        bool searchPlace = false;
-        int newPlace = 0;
-        int finalScore = gameScore;
-        int[] scoreBoard = new int[12];
+        bool searchHighScoreSpot = false;
+        int newHighScoreSpot = 0;
+        int finalGameScore = currentGameScore;
+        int[] newHighScoreBoard = new int[12];
         for (int i=0; i<10; i++)
         {
-            scoreBoard[i] = gameScore[i];
+            newHighScoreBoard[i] = highScoresBoard[i];
         }
 
         //Is current game score worthy of High Scores?
         //yaas
-        if (finalScore > scoreBoard[9])
+        if (finalGameScore > newHighScoreBoard[9])
         {
-            searchPlace = true;
-            while(searchPlace==true)
+            searchHighScoreSpot = true;
+            while(searchHighScoreSpot==true)
             {
                 for(int i=8; i>=0; i--)
                 {
-                    if(scoreBoard[i]>=finalScore)
+                    if(newHighScoreBoard[i]>=finalGameScore)
                     {
-                        newPlace = i + 1;
-                        searchPlace = false;
+                        newHighScoreSpot = i + 1;
+                        searchHighScoreSpot = false;
                     }
                         //NEW HIGH SCORE #1!!!
-                    else if (i==0 && finalScore>scoreBoard[i])
+                    else if (i==0 && finalGameScore>newHighScoreBoard[i])
                     {
-                        searchPlace = false;
+                        searchHighScoreSpot = false;
                     }
                 }
             }
             //recalculate all scores @board
-            if(newPlace<9)
+            if(newHighScoreSpot<9)
             {
-                for (int i = 8; i <= newPlace; i--)
+                for (int i = 8; i <= newHighScoreSpot; i--)
                 {
-                    scoreBoard[i+1] = scoreBoard[i];
+                    newHighScoreBoard[i+1] = newHighScoreBoard[i];
                 }
             }
-            scoreBoard[newPlace] = finalScore;
-            scoreBoard[11] = newPlace;
-            return scoreBoard;
+            newHighScoreBoard[newHighScoreSpot] = finalGameScore;
+            newHighScoreBoard[11] = newHighScoreSpot;
+            return newHighScoreBoard;
         }
 
         //nope
         else
         {
-            scoreBoard(10) = finalScore;
-            scoreBoard(11) = 10;
-            return scoreBoard;
+            newHighScoreBoard[10] = finalGameScore;
+            newHighScoreBoard[11] = 10;
+            return newHighScoreBoard;
         }
     }
 }
