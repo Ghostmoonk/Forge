4cea7650d456737d74f7a166b46f369b0e808195
Martin <valleixmartin89@gmail.com>
2020-02-02 04:56:53 +0100

    SceneManager

--- Assets/Scripts/Manager/_MGR_SceneManager.cs
+++ Assets/Scripts/Manager/_MGR_SceneManager.cs
@@ -10,7 +10,6 @@ public class _MGR_SceneManager : MonoBehaviour
 
     public string[] arr_SceneName;
 
-    private 
 
     void Awake()
     {
@@ -23,6 +22,8 @@ public class _MGR_SceneManager : MonoBehaviour
         else if (p_instance != this)
             //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
             Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
     }
     // Start is called before the first frame update
     void Start()
@@ -39,5 +40,12 @@ public class _MGR_SceneManager : MonoBehaviour
     public void LoadScene(string __nom_scene)
     {
         SceneManager.LoadScene(__nom_scene);
+    }
+
+    public void LoadEndScene(string __nom_scene)
+    {
+        SceneManager.LoadScene(__nom_scene);
+        GameObject panel = GameObject.Find("FonctionUI").GetComponent<UIFonction>().panel;
+        panel.SetActive(true);
     }
 }
