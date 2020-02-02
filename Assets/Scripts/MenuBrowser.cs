7e735a72b5177f9d10f85740cfcfa3bf3d42a76d
Ghostmoonk <jeremy.tapia30@hotmail.com>
2020-02-02 12:36:52 +0100

    Select un bouton de base sur le menu

---
+++ Assets/Scripts/MenuBrowser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBrowser : MonoBehaviour
+{
+    [SerializeField] Button[] menuButtons;
+
+    void Start()
+    {
+        menuButtons[0].Select();
+    }
+
+}
